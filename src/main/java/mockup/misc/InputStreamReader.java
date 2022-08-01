e694ae3375f6e6274da4e026b40421f0822449b6
Chuangjie Xu <cj-xu@outlook.com>
2022-08-01 11:15:08 +0200

    add a constructor for the mockuped InputStreamReader to support the mockup of Socket

--- src/main/java/mockup/misc/InputStreamReader.java
+++ src/main/java/mockup/misc/InputStreamReader.java
@@ -5,11 +5,17 @@ import mockup.servlet.DummyServletInputStream;
 
 import java.io.Reader;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 
 @Replaces("java.io.InputStreamReader")
 abstract class InputStreamReader extends Reader {
 	public String s;
 	public InputStreamReader(InputStream in){
+		s = ((DummyServletInputStream) in).s;
+	}
+
+	public InputStreamReader(InputStream in, String charsetName)
+			throws UnsupportedEncodingException {
 		s = ((DummyServletInputStream) in).s;
 	}
 }
